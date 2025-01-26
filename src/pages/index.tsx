a65c876cb497ad92e07f568065e8a250e2329e36
aliseena1 <aliseena@live.unc.edu>
2025-01-26 18:12:46 -0500

    Completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Profile from './components/profile_props'
+import Gallery from './qcomps/gallery_props'
+import Holder from './components/square'
+import RecipeList from './qcomps/recipes'
+import List from './qcomps/list_keys_id'
 
 export default function Home() {
   return (
-    <Profile />
+    <List />
   )
 }
--- src/pages/qcomps/recipes.tsx
+++ src/pages/qcomps/recipes.tsx
@@ -1,3 +1,15 @@
+// Recipe item interface
+interface Recipe {
+  id: string;
+  name: string;
+  ingredients: Set<string>;
+}
+
+// Interface for RecipeItem
+interface RecipeItemProps {
+  recipe: Recipe
+}
+
 export const recipes = [{
   id: 'greek-salad',
   name: 'Greek Salad',
@@ -12,11 +24,27 @@ export const recipes = [{
   ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
 }];
 
+// Function to implement a recipe item
+function RecipeItem({recipe}: RecipeItemProps) {
+  const recipeArray = Array.from(recipe.ingredients);
+
+  return (
+    <div>
+      <b>{recipe.name}</b>
+      {recipeArray.map((ingredient) => (
+        <li key={ingredient}>{ingredient}</li>
+      ))}
+    </div>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      {recipes.map(recipe => (
+        <RecipeItem key={recipe.id} recipe={recipe} />
+      ))}
     </div>
   );
 }
